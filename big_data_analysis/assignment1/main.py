9e645eabc98d98caac31c19dd7b2b2d9f539a9d6
Aleksandr J. Smoliakov <aleksandr.smoliakov@euromonitor.com>
2025-03-12 00:59:26 +0200

    final?

--- big_data_analysis/assignment1/main.py
+++ big_data_analysis/assignment1/main.py
@@ -2,37 +2,48 @@ import math
 import time
 import logging
 import multiprocessing as mp
-from dataclasses import dataclass
 import numpy as np
 import pandas as pd
+from collections import namedtuple
+from dataclasses import dataclass
 
-CHUNK_SIZE = 1_000_000
-N_PARSING_PROCESSES = max(mp.cpu_count() - 2, 1)
-COLUMNS = ["# Timestamp", "MMSI", "Latitude", "Longitude", "SOG", "COG"]
+CHUNK_SIZE = 100_000
+N_PARSING_PROCESSES = max(mp.cpu_count() - 1, 1)
+COLUMNS = ["# Timestamp", "MMSI", "Latitude", "Longitude"]
+
+
+GpsPointDelta = namedtuple("GpsPointDelta", ["distance", "time_delta"])
 
 
 @dataclass
-class VesselState:
-    mmsi: int
+class GpsPoint:
     timestamp: pd.Timestamp
     latitude: float
     longitude: float
-    sog: float
-    cog: float
 
-    def update(
-        self,
-        timestamp: pd.Timestamp,
-        latitude: float,
-        longitude: float,
-        sog: float,
-        cog: float,
-    ) -> None:
-        self.timestamp = timestamp
-        self.latitude = latitude
-        self.longitude = longitude
-        self.sog = sog
-        self.cog = cog
+    def __sub__(self, other: "GpsPoint") -> float:
+        distance = haversine_distance(
+            self.latitude, self.longitude, other.latitude, other.longitude
+        )
+        time_delta = abs((self.timestamp - other.timestamp).total_seconds())
+
+        return GpsPointDelta(distance=distance, time_delta=time_delta)
+
+
+class VesselState:
+    def __init__(self, gps_point: GpsPoint):
+        self.last_point = gps_point
+        self.possible_locations = [gps_point]
+
+    def update(self, gps_point: GpsPoint):
+        self.last_point = gps_point
+
+        for idx, other_location in enumerate(self.possible_locations):
+            if (gps_point - other_location).distance <= 20_000:
+                self.possible_locations[idx] = gps_point
+                break
+        else:
+            self.possible_locations.append(gps_point)
 
 
 def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
@@ -50,143 +61,75 @@ def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> fl
     return c * earth_radius
 
 
-def check_location_anomaly(distance: float, time_diff: float) -> bool:
-    """Return True if the distance exceeds 150 km in under 1 minute."""
-    return time_diff < 60 and distance > 150_000
-
-
-def detect_duplicate_mmsi(
-    df: pd.DataFrame,
-    eps: float = 2000.0,
-) -> pd.DataFrame:
-    """
-    Detect duplicate MMSI records within a given time window.
-
-    Args:
-        df: DataFrame containing columns ["Timestamp", "MMSI", "Latitude", "Longitude"].
-        eps: The maximum distance for connecting the positions.
-
-    Returns:
-        TODO
-    """
-    suspicious_records = []
-    for mmsi, group_df in df.groupby("MMSI"):
-        if len(group_df) < 2:
-            continue
-
-        coords = list(zip(group_df["Latitude"], group_df["Longitude"]))
-        last_locations = []
-        for row in group_df.itertuples(index=False):
-            latitude, longitude = row.Latitude, row.Longitude
-            for idx, last_location in enumerate(last_locations):
-                dist = haversine_distance(
-                    latitude, longitude, last_location[0], last_location[1]
-                )
-                if dist <= eps:
-                    last_locations[idx] = (latitude, longitude)
-                    break
-            else:
-                last_locations.append((latitude, longitude))
-
-        if len(last_locations) > 1:
-            logging.warning(f"Found {len(last_locations)} locations for MMSI {mmsi}")
-
-            suspicious_records.append(
-                {
-                    "latitude": None,
-                    "longitude": None,
-                    "mmsi": mmsi,
-                    "timestamp": None,
-                    "reason": f"Suspected {len(last_locations)} ships under one MMSI",
-                }
-            )
-
-    return suspicious_records
-
-
-def parse_chunk(chunk_df: pd.DataFrame, vessel_states: dict) -> list[dict]:
+def parse_chunk(
+    chunk_df: pd.DataFrame, vessel_states: dict, suspicious_mmsi: set
+) -> list[dict]:
     """Process a chunk of data, updating vessel states and collecting alerts."""
     alerts = []
-    suspicious_mmsi = set()
 
     for row in chunk_df.itertuples(index=False):
         mmsi = row.MMSI
+        gps_point = GpsPoint(
+            timestamp=row.Timestamp,
+            latitude=row.Latitude,
+            longitude=row.Longitude,
+        )
+
         if mmsi not in vessel_states:
-            vessel_states[mmsi] = VesselState(
-                mmsi=mmsi,
-                timestamp=row.Timestamp,
-                latitude=row.Latitude,
-                longitude=row.Longitude,
-                sog=row.SOG,
-                cog=row.COG,
-            )
+            vessel_states[mmsi] = VesselState(gps_point)
             continue
 
-        state = vessel_states[mmsi]
-        time_diff = max((row.Timestamp - state.timestamp).total_seconds(), 1)
-        distance = haversine_distance(
-            state.latitude, state.longitude, row.Latitude, row.Longitude
-        )
-        speed = distance / time_diff
+        point_delta = gps_point - vessel_states[mmsi].last_point
+        speed = point_delta.distance / max(point_delta.time_delta, 1)
 
-        if check_location_anomaly(distance, time_diff):
+        if point_delta.distance > 20_000:
+            alert = {
+                "mmsi": mmsi,
+                "latitude": row.Latitude,
+                "longitude": row.Longitude,
+                "timestamp": row.Timestamp,
+                "reason": f"Location jump of {point_delta.distance:.2f}m",
+            }
+            logging.warning(alert["reason"])
+            alerts.append(alert)
+
             suspicious_mmsi.add(mmsi)
-            logging.warning(f"Location anomaly: {distance:.2f}m in {time_diff:.2f}s")
-            alerts.append(
-                {
-                    "latitude": row.Latitude,
-                    "longitude": row.Longitude,
-                    "mmsi": mmsi,
-                    "timestamp": row.Timestamp,
-                    "reason": f"Location jump of {distance:.2f}m in {time_diff:.2f}s",
-                }
-            )
 
         if speed > 200:
-            suspicious_mmsi.add(mmsi)
-            logging.warning(f"Speed anomaly: {speed:.2f} m/s")
-            alerts.append(
-                {
-                    "latitude": row.Latitude,
-                    "longitude": row.Longitude,
-                    "mmsi": mmsi,
-                    "timestamp": row.Timestamp,
-                    "reason": f"Speed {speed:.2f} m/s > 200 m/s",
-                }
-            )
+            alert = {
+                "mmsi": mmsi,
+                "latitude": row.Latitude,
+                "longitude": row.Longitude,
+                "timestamp": row.Timestamp,
+                "reason": f"Speed {speed:.2f} m/s > 200 m/s",
+            }
+            logging.warning(alert["reason"])
+            alerts.append(alert)
 
-        state.update(row.Timestamp, row.Latitude, row.Longitude, row.SOG, row.COG)
+        vessel_states[mmsi].update(gps_point)
 
-    # Check for duplicate MMSI entries within a 10-minute window.
-    duplicate_alerts = detect_duplicate_mmsi(
-        df=chunk_df[chunk_df["MMSI"].isin(suspicious_mmsi)],
-        eps=2000.0,
-    )
-    alerts.extend(duplicate_alerts)
     return alerts
 
 
-def reader_process(
-    file_path: str, chunk_size: int, task_queues: list[mp.Queue]
-) -> None:
+def reader_process(file_path: str, task_queues: list[mp.Queue]) -> None:
     """Read CSV data in chunks and distribute tasks to worker queues."""
-    for chunk in pd.read_csv(
+    for chunk_df in pd.read_csv(
         file_path,
         usecols=COLUMNS,
         parse_dates=["# Timestamp"],
         dayfirst=True,
-        chunksize=chunk_size,
-        nrows=CHUNK_SIZE * 2,
+        chunksize=CHUNK_SIZE,
+        # nrows=CHUNK_SIZE * 5,
     ):
-        chunk.rename(columns={"# Timestamp": "Timestamp"}, inplace=True)
-        # Assign a worker based on a hash of MMSI.
-        chunk["worker_id"] = chunk["MMSI"] % 31337 % N_PARSING_PROCESSES
+        chunk_df.rename(columns={"# Timestamp": "Timestamp"}, inplace=True)
+        # assign worker based on a deterministic hash of MMSI
+        chunk_df["worker_id"] = chunk_df["MMSI"] % 31337 % N_PARSING_PROCESSES
 
-        for worker_id, group in chunk.groupby("worker_id"):
+        for worker_id, group_df in chunk_df.groupby("worker_id"):
             queue = task_queues[worker_id]
-            queue.put(group.drop("worker_id", axis=1))
-            logging.info(
-                f"Queued {len(group)} rows to worker {worker_id} (queue size: {queue.qsize()})"
+            queue.put(group_df.drop("worker_id", axis=1))
+            logging.debug(
+                f"Queued {len(group_df)} rows to worker {worker_id} (queue size: {queue.qsize()})"
             )
 
     # Signal workers to exit.
@@ -197,18 +140,33 @@ def reader_process(
 def worker_process(task_queue: mp.Queue, result_queue: mp.Queue) -> None:
     """Worker process to parse data chunks and report alerts."""
     vessel_states = {}
+    suspicious_mmsi = set()
     while True:
         start_time = time.time()
-        chunk = task_queue.get()
+        chunk_df = task_queue.get()
         wait_time = time.time() - start_time
-        if wait_time > 0.1:
-            logging.warning(f"Queue wait time: {wait_time:.2f}s")
 
-        if chunk is None:
+        if wait_time > 0.5:
+            logging.warning(f"Task queue wait time: {wait_time:.2f}s")
+
+        if chunk_df is None:
+            alerts = []
+            for mmsi, state in vessel_states.items():
+                if len(state.possible_locations) > 1:
+                    alert = {
+                        "mmsi": mmsi,
+                        "latitude": None,
+                        "longitude": None,
+                        "timestamp": None,
+                        "reason": f"Suspected {len(state.possible_locations)} ships under one MMSI",
+                    }
+                    logging.warning(alert["reason"])
+
+            result_queue.put(alerts)
             result_queue.put(None)
             break
 
-        alerts = parse_chunk(chunk, vessel_states)
+        alerts = parse_chunk(chunk_df, vessel_states, suspicious_mmsi)
         result_queue.put(alerts)
 
 
@@ -217,12 +175,10 @@ def main() -> None:
     task_queues = [mp.Queue(maxsize=10) for _ in range(N_PARSING_PROCESSES)]
     result_queue = mp.Queue()
 
-    reader = mp.Process(
-        target=reader_process, args=(file_path, CHUNK_SIZE, task_queues)
-    )
+    reader = mp.Process(target=reader_process, args=(file_path, task_queues))
     workers = [
-        mp.Process(target=worker_process, args=(queue, result_queue))
-        for queue in task_queues
+        mp.Process(target=worker_process, args=(task_queue, result_queue))
+        for task_queue in task_queues
     ]
 
     reader.start()
